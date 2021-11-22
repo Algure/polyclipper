816031303174ce39264cb9b1f0d08a0080732336
Algure <ajirie98@gmail.com>
2021-11-22 01:22:14 +0100

    Created example.

--- lib/clippers/poly_con_clipper.dart
+++ lib/clippers/poly_con_clipper.dart
@@ -13,16 +13,17 @@ class PolyContainerDynamicClipper extends CustomClipper<Path>{
   @override
   Path getClip(Size size) {
     var path = Path();
-    // double totalAngles = 360 / sides; // (sides-2) * 180;
-    double angleDif = 360/sides;
+    final double angleDif = 360/sides;
     List<double> angleList = [];
 
+    // Obtain polygon angles
     double angle = 0;
     while(angle < 360){
       angleList.add(angle+startAngle);
       angle += angleDif;
     }
 
+    // Initialise circle defaults
     double radius = min(size.width,size.height)/2;
     double x0 = size.width / 2;
     double y0 = size.height / 2;
@@ -30,7 +31,6 @@ class PolyContainerDynamicClipper extends CustomClipper<Path>{
     double x = radius ;
 
     List<List<double>> xyPoints = [];
-    print('radius: $radius');
     for(double angle in angleList){
       x = radius* cos(getInRads(angle));
       x = x0+x;
@@ -39,19 +39,14 @@ class PolyContainerDynamicClipper extends CustomClipper<Path>{
       y = getYOnCircle(radius, x0, y0, x, angle);
       y= y0-y;
       tempArray.add(y);
-      print('x: $x, angle: $angle, y:$y ');
       xyPoints.add(tempArray);
     }
     xyPoints.add(xyPoints[0]);
 
-    print('${angleList.length} angleList len: xyPoints length: ${xyPoints.length}');
-    print('point 0 x: ${xyPoints[0][0]} , y: ${xyPoints[0][1]}');
     path.moveTo(xyPoints[0][0], xyPoints[0][1]);
     for(List<double> point in xyPoints){
       path.lineTo(point[0], point[1]);
-      print('point i x: ${point[0]} , y: ${point[1]}');
     }
-
     path.close();
 
     return path;
@@ -66,9 +61,9 @@ class PolyContainerDynamicClipper extends CustomClipper<Path>{
 
   double getYOnCircle(double radius, double x0, double y0, double x, double angle) {
     final sinAngle = sin(getInRads(angle));
-    final double answer = (radius*radius)-pow((x -x0),2) ;
-
-    return sinAngle<0? -1* sqrt(answer): sqrt(answer);
+    final double answerSq = (radius*radius)-pow((x -x0),2) ;
+    final answer = sqrt(answerSq);
+    return sinAngle<0 ? -1*  answer: answer;
   }
 
 }
--- lib/polycontainer.dart
+++ lib/polycontainer.dart
@@ -1,18 +1,22 @@
 library polycontainer;
 
 import 'package:flutter/material.dart';
+import 'package:polycontainer/clippers/poly_con_clipper.dart';
 
 
 class ClipPoly extends StatelessWidget {
   double shift;
   int sides;
   Widget child;
-   ClipPoly({required this.sides, required this.shift, required this.child });
+
+   ClipPoly({required this.sides, this.shift=0, required this.child }){
+     assert( sides >=3 );
+   }
 
   @override
   Widget build(BuildContext context) {
     return ClipPath(
-      clipper: ,
+      clipper: PolyContainerDynamicClipper(sides: sides, startAngle:shift ),
       child: this.child,
     );
   }
